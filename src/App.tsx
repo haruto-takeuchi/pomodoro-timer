208853dd45779baefc70a4fc1fc4841f4511989d
haruto-takeuchi <haruto.takeuchi@esm.co.jp>
2024-02-06 00:58:09 +0900

    ポモドーロタイマー作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useTimer } from 'react-timer-hook'
 import './App.css'
+import { CustomButton } from './components/CutomButton'
+import { useState, useEffect } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const PomodoroTimer = () => {
+  const workDuration = 25 * 60 // 25 minutes
+  const breakDuration = 5 * 60 // 5 minutes
+  const [isWorkTime, setIsWorkTIme] = useState(true)
+  const enableAutoStart = false
+
+  const getExpiryTimestamp = (): Date => {
+    const time = new Date()
+    time.setSeconds(
+      time.getSeconds() + (isWorkTime ? workDuration : breakDuration)
+    )
+    return time
+  }
+
+  const { seconds, minutes, isRunning, pause, resume, restart } = useTimer({
+    expiryTimestamp: getExpiryTimestamp(),
+    autoStart: enableAutoStart,
+    onExpire: () => setIsWorkTIme(!isWorkTime),
+  })
+
+  useEffect(() => {
+    restart(getExpiryTimestamp(), enableAutoStart)
+  }, [isWorkTime])
+
+  const start = () => {
+    if (isRunning) return
+    resume()
+  }
+
+  const stop = () => {
+    if (!isRunning) return
+    pause()
+  }
+
+  // 0詰め
+  const formattesMunites = minutes.toString().padStart(2, '0')
+  const formattesSeconds = seconds.toString().padStart(2, '0')
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="text-[12rem] font-bold mb-8">
+        <span>{formattesMunites}</span>:<span>{formattesSeconds}</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="flex justify-center gap-8">
+        <CustomButton onClick={start}>Start</CustomButton>
+        <CustomButton onClick={stop}>Stop</CustomButton>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+      <PomodoroTimer />
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,js,ts,jsx,tsx}"
-  ],
+  content: ['./index.html', './src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ffffffde',
+        secondary: '#242424',
+      },
+    },
   },
   plugins: [],
 }
