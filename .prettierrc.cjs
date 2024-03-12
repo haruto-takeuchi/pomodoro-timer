5247476fb240b7a3e7b0c0c08c89643d5936fb5b
haruto-takeuchi <haruto.takeuchi@esm.co.jp>
2024-03-13 00:30:21 +0900

    Prettierバージョンアップ対応

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -5,4 +5,12 @@ module.exports = {
   trailingComma: 'es5',
   tabWidth: 2,
   endOfLine: 'auto',
+  overrides: [
+    {
+      files: ['tsconfig.json', 'jsconfig.json'],
+      options: {
+        parser: 'jsonc',
+      },
+    },
+  ],
 }
